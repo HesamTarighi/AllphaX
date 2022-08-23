bca2f3f48885e05a127e6c68280d9c5051fc5419
Hesam <hesammp86@gmail.com>
2022-08-24 02:38:19 +0330

    AllphaX

---
+++ .nuxt/dist/server/components/nav-header.js
@@ -0,0 +1,69 @@
+exports.ids = [3];
+exports.modules = {
+
+/***/ 53:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/NavHeader.vue?vue&type=template&id=5422535e&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_vm._ssrNode("<nav class=\"flex justify-center space-x-10 uppercase\"><ul class=\"flex justify-center text-lg text-black-100\">"+(_vm._ssrList((_vm.lists),function(list,index){return ("<li class=\"px-5 cursor-pointer\">"+_vm._ssrEscape(" "+_vm._s(list.item)+" ")+"</li>")}))+"</ul> <div class=\"w-400 header-search-box\"><input type=\"text\" placeholder=\"Search...\" class=\"w-full rounded-full shadow-md py-1 px-3\"></div> <div class=\"justify-self-end\"><button class=\"rounded bg-blue-500 shadow-md px-3 py-1 uppercase\">Sign in</button></div></nav>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./components/NavHeader.vue?vue&type=template&id=5422535e&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/NavHeader.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var NavHeadervue_type_script_lang_js_ = ({
+  props: {
+    lists: Object
+  }
+});
+// CONCATENATED MODULE: ./components/NavHeader.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_NavHeadervue_type_script_lang_js_ = (NavHeadervue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/NavHeader.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_NavHeadervue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "14258fca"
+  
+)
+
+/* harmony default export */ var NavHeader = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=nav-header.js.map
---
+++ .nuxt/dist/server/components/ready-project.js
@@ -0,0 +1,94 @@
+exports.ids = [5];
+exports.modules = {
+
+/***/ 41:
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(49);
+if(content.__esModule) content = content.default;
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add CSS to SSR context
+var add = __webpack_require__(4).default
+module.exports.__inject__ = function (context) {
+  add("5a7f8eae", content, true, context)
+};
+
+/***/ }),
+
+/***/ 48:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_nuxt_postcss8_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_nuxt_postcss8_node_modules_postcss_loader_dist_cjs_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ReadyProject_vue_vue_type_style_index_0_id_38e76148_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(41);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_nuxt_postcss8_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_nuxt_postcss8_node_modules_postcss_loader_dist_cjs_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ReadyProject_vue_vue_type_style_index_0_id_38e76148_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_nuxt_postcss8_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_nuxt_postcss8_node_modules_postcss_loader_dist_cjs_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ReadyProject_vue_vue_type_style_index_0_id_38e76148_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_nuxt_postcss8_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_nuxt_postcss8_node_modules_postcss_loader_dist_cjs_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ReadyProject_vue_vue_type_style_index_0_id_38e76148_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_nuxt_postcss8_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_nuxt_postcss8_node_modules_postcss_loader_dist_cjs_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ReadyProject_vue_vue_type_style_index_0_id_38e76148_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+
+
+/***/ }),
+
+/***/ 49:
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(3);
+var ___CSS_LOADER_EXPORT___ = ___CSS_LOADER_API_IMPORT___(function(i){return i[1]});
+// Module
+___CSS_LOADER_EXPORT___.push([module.i, "/*purgecss start ignore*/\nsection[data-v-38e76148]{\n  background-color:#121934\n}\n\n/*purgecss end ignore*/", ""]);
+// Exports
+___CSS_LOADER_EXPORT___.locals = {};
+module.exports = ___CSS_LOADER_EXPORT___;
+
+
+/***/ }),
+
+/***/ 56:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/ReadyProject.vue?vue&type=template&id=38e76148&scoped=true&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('section',{staticClass:"flex justify-center overflow-hidden"},[_vm._ssrNode("<div class=\"w-4/5 flex\" data-v-38e76148><div class=\"flex items-center\" data-v-38e76148><div data-v-38e76148><strong class=\"text-8xl text-imp\" data-v-38e76148>\n          Allpha\n        </strong></div> <div class=\"w-96 -my-4\" data-v-38e76148><img src=\"/image/illestrator/x.png\" alt data-v-38e76148></div></div> <div class=\"flex flex-col justify-center py-16 px-8 space-y-16\" data-v-38e76148><div data-v-38e76148><strong class=\"flex flex-col text-4xl\" data-v-38e76148><span class=\"text-imp\" data-v-38e76148>Ready To Discuss Your</span> <span class=\"text-imp\" data-v-38e76148>Project?</span></strong></div> <div data-v-38e76148> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aperiam esse illum neque quae sunt, consequuntur quaerat, odit voluptatibus molestias vel ab quisquam hic repellendus totam voluptatem saepe illo ipsam. </div> <div class=\"flex justify-center\" data-v-38e76148><button class=\"px-8 py-2 bg-imp rounded-full text-lg\" data-v-38e76148> Get Started </button></div></div></div>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./components/ReadyProject.vue?vue&type=template&id=38e76148&scoped=true&
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/ReadyProject.vue
+
+var script = {}
+function injectStyles (context) {
+  
+  var style0 = __webpack_require__(48)
+if (style0.__inject__) style0.__inject__(context)
+
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  script,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  "38e76148",
+  "1889debb"
+  
+)
+
+/* harmony default export */ var ReadyProject = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=ready-project.js.map
