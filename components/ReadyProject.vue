04f716df51fe91550f556e86eda6c83358ad5a9f
Hesam <hesammp86@gmail.com>
2022-12-07 02:22:54 +0330

    Changed

--- components/ReadyProject.vue
+++ components/ReadyProject.vue
@@ -1,24 +1,24 @@
 <template>
   <section class="flex justify-center overflow-hidden">
-    <div class="w-4/5 flex">
+    <div class="xl:w-4/5 lg:w-full sm:w-4/5 xs:w-full flex">
       <div class="flex items-center">
         <div>
-          <strong class="text-8xl text-imp">
+          <strong class="2xl:text-8xl lg:text-7xl lg:block xs:hidden text-imp">
             Allpha
           </strong>
         </div>
-        <div class="w-96 -my-4"><img src="/image/illestrator/x.png" alt=""></div>
+        <div class="w-96 -my-4 lg:block xs:hidden"> <img src="/image/illestrator/x.png" class="xl:h-[600px]"> </div>
       </div>
       <div class="flex flex-col justify-center py-16 px-8 space-y-16">
         <div>
-          <strong class="flex flex-col text-4xl">
+          <strong class="flex flex-col 2xl:text-4xl lg:text-2xl sm:text-3xl xs:text-xl">
             <span class="text-imp">Ready To Discuss Your</span>
             <span class="text-imp">Project?</span>
           </strong>
         </div>
         <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aperiam esse illum neque quae sunt, consequuntur quaerat, odit voluptatibus molestias vel ab quisquam hic repellendus totam voluptatem saepe illo ipsam. </div>
         <div class="flex justify-center">
-          <button class="px-8 py-2 bg-imp rounded-full text-lg"> Get Started </button>
+          <button class="xl:px-8 xs:px-6 xs:py-2 xs:text-sm lg:text-lg bg-imp rounded-full"> Get Started </button>
         </div>
       </div>
     </div>
