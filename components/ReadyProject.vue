e4c9dbdb5526d9a03f816e26fe9fd37d8282343f
Hesam <hesammp86@gmail.com>
2022-12-08 00:33:08 +0330

    Changed

--- components/ReadyProject.vue
+++ components/ReadyProject.vue
@@ -9,9 +9,9 @@
         </div>
         <div class="w-96 -my-4 lg:block xs:hidden"> <img src="/image/illestrator/x.png" class="xl:h-[600px]"> </div>
       </div>
-      <div class="flex flex-col justify-center py-16 px-8 space-y-16">
+      <div class="flex flex-col justify-center py-16 xs:px-8 xl:px-0 space-y-16">
         <div>
-          <strong class="flex flex-col 2xl:text-4xl lg:text-2xl sm:text-3xl xs:text-xl">
+          <strong class="flex flex-col 2xl:text-4xl xl:text-3xl lg:text-2xl sm:text-3xl xs:text-xl">
             <span class="text-imp">Ready To Discuss Your</span>
             <span class="text-imp">Project?</span>
           </strong>
--- components/Services.vue
+++ components/Services.vue
@@ -6,22 +6,30 @@
         <span class="text-imp">Services</span>
       </strong>
       <div class="grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-6">
-        <div class="bg-white flex flex-col items-center rounded-xl shadow-2xl xl:px-8 sm:px-4 xs:px-3 py-8 space-y-5">
-          <div class="sm:w-32 xs:w-28"> <img src="/image/illestrator/seo.png" alt=""> </div>
-          <div class="text-gray-900 2xl:text-2xl xs:text-xl">Seo</div>
-          <div class="text-gray-700 text-center xl:text-lg xs:text-md opacity-70 thin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat odit laborum totam corporis, dolor ratione, cumque dignissimos, numquam pariatur est itaque. Saepe recusandae, ducimus veniam nostrum modi quia a inventore?</div>
-        </div>
-        <div class="bg-white flex flex-col items-center rounded-xl shadow-2xl xl:px-8 sm:px-4 xs:px-3 py-8 space-y-5">
-          <div class="sm:w-32 xs:w-28"> <img src="/image/illestrator/uiux.png" alt=""> </div>
-          <div class="text-gray-900 2xl:text-2xl xs:text-xl">UI & UX Designer</div>
-          <div class="text-gray-700 text-center xl:text-lg xs:text-md opacity-70 thin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat odit laborum totam corporis, dolor ratione, cumque dignissimos, numquam pariatur est itaque. Saepe recusandae, ducimus veniam nostrum modi quia a inventore?</div>
-        </div>
-        <div class="bg-white flex flex-col items-center rounded-xl shadow-2xl xl:px-8 sm:px-4 xs:px-3 py-8 space-y-5">
-          <div class="sm:w-32 xs:w-28"> <img src="/image/illestrator/webdesign.png" alt=""> </div>
-          <div class="text-gray-900 2xl:text-2xl xs:text-xl">Web Design</div>
-          <div class="text-gray-700 text-center xl:text-lg xs:text-md opacity-70 thin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat odit laborum totam corporis, dolor ratione, cumque dignissimos, numquam pariatur est itaque. Saepe recusandae, ducimus veniam nostrum modi quia a inventore?</div>
+        <div
+        class="bg-white flex flex-col items-center rounded-xl shadow-2xl xl:px-8 sm:px-4 xs:px-3 py-8 space-y-5"
+        v-for="(service, i) in services"
+        :key="i"
+        >
+          <div class="sm:w-32 xs:w-28"> <img :src="service.servicePicture" alt=""> </div>
+          <div class="text-gray-900 2xl:text-2xl xs:text-xl"> {{ service.serviceName }} </div>
+          <div class="text-gray-700 text-center xl:text-lg xs:text-md opacity-70 thin"> {{ service.serviceDescription }} </div>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        services: [
+          {serviceName: 'Seo', serviceDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat odit laborum totam corporis, dolor ratione', servicePicture: '/image/illestrator/seo.png'},
+          {serviceName: 'UI & UX Designer', serviceDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat odit laborum totam corporis, dolor ratione', servicePicture: '/image/illestrator/uiux.png'},
+          {serviceName: 'Web Design', serviceDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat odit laborum totam corporis, dolor ratione', servicePicture: '/image/illestrator/webdesign.png'},
+        ],
+      }
+    }
+  }
+</script>
